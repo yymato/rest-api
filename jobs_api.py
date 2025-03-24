dfe7127963b9095489f2d40f8c9908b3027bb219
yymato <070913a@gmail.com>
2025-03-24 15:12:34 +0300

    Задача Добавление работы. Все остальные апи переделаны на одну ссылку /api/jobs

--- jobs_api.py
+++ jobs_api.py
@@ -1,5 +1,7 @@
 import flask
+import requests
 from flask import jsonify, make_response
+from flask import request
 
 from data.db_session import create_session
 from data.users import Jobs
@@ -9,8 +11,9 @@ blueprint = flask.Blueprint(
     __name__,
 )
 
-@blueprint.route('/api/job/<int:job_id>')
-def get_job(job_id):
+
+@blueprint.route('/api/jobs/<int:job_id>', methods=['GET'])
+def get_one_job(job_id):
     db_session = create_session()
     job = db_session.query(Jobs).get(job_id)
     if job:
@@ -19,18 +22,40 @@ def get_job(job_id):
         return make_response(jsonify({'error': 'Job not found'}, 404))
 
 
-@blueprint.route('/api/jobs')
-def get_jobs():
+@blueprint.route('/api/jobs', methods=['GET'])
+def get_all_jobs():
     db_session = create_session()
-    return jsonify(
-        {
-            'Jobs': [i.to_dict() for i in db_session.query(Jobs).all()]
-        }
-    )
+    return jsonify({'Jobs': [i.to_dict() for i in db_session.query(Jobs).all()]})
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def add_job():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+
+    db_session = create_session()
+
+    if all([key in request.json for key in
+            ['team_leader', 'job', 'work_size', 'collaborators', 'is_finished']]):
+
+        job = Jobs()
+        job.team_leader = request.json['team_leader']
+        job.job = request.json['job']
+        job.work_size = request.json['work_size']
+        job.collaborators = request.json['collaborators']
+        job.is_finished = request.json['is_finished']
+
+        db_session.add(job)
+        db_session.commit()
+        return make_response(jsonify({'success': True}))
+    else:
+        return make_response(jsonify({'error': 'Bad request'}), 404)
+
 
 @blueprint.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @blueprint.errorhandler(400)
 def bad_request(_):
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-from requests import get
+from requests import get, post
 
 print(get('http://localhost:5000/api/jobs').json(), 3 * '\n')  # Получение всех работ
-print(get('http://localhost:5000/api/job/2').json(), 3 * '\n')  # Корректное получение одной работы
-print(get('http://localhost:5000/api/job/999999').json(), 3 * '\n')  # Ошибочный запрос на получение одной работы — неверный id
-print(get('http://localhost:5000/api/job/q').json(), 3 * '\n')  # Ошибочный запрос на получение одной работы — строка
+print(get('http://localhost:5000/api/jobs/2').json(), 3 * '\n')  # Корректное получение одной работы
+print(get('http://localhost:5000/api/jobs/999999').json(), 3 * '\n')  # Ошибочный запрос на получение одной работы — неверный id
+print(get('http://localhost:5000/api/jobs/q').json(), 3 * '\n')  # Ошибочный запрос на получение одной работы — строка
+
+
+print(post('http://localhost:5000/api/jobs', json={}).json(), 3 * '\n')
